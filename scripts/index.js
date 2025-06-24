e3f734173fdb59d36b5c5401361ce8e55d763a4c
Morqqulis <morqqulis@gmail.com>
2025-06-24 17:21:57 +0400

    cc

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,26 @@
 'use strict'
 
 const keys = document.querySelectorAll('.key')
+const pads = [
+	{ letter: 'A', sound: 'clap', src: '/assets/sounds/clap.wav' },
+	{ letter: 'S', sound: 'hihat', src: '/assets/sounds/hihat.wav' },
+	{ letter: 'D', sound: 'kick', src: '/assets/sounds/kick.wav' },
+	{ letter: 'Z', sound: 'openhat', src: '/assets/sounds/openhat.wav' },
+	{ letter: 'X', sound: 'boom', src: '/assets/sounds/boom.wav' },
+	{ letter: 'C', sound: 'ride', src: '/assets/sounds/ride.wav' },
+	{ letter: 'V', sound: 'snare', src: '/assets/sounds/snare.wav' },
+	{ letter: 'G', sound: 'tom', src: '/assets/sounds/tom.wav' },
+	{ letter: 'H', sound: 'tink', src: '/assets/sounds/tink.wav' },
+]
 
 // Создаем объект для хранения аудио элементов
-const audioElements = {
-	A: new Audio('./assets/sounds/clap.wav'),
-	S: new Audio('./assets/sounds/hihat.wav'),
-	D: new Audio('./assets/sounds/kick.wav'),
-	Z: new Audio('./assets/sounds/openhat.wav'),
-	X: new Audio('./assets/sounds/boom.wav'),
-	C: new Audio('./assets/sounds/ride.wav'),
-	V: new Audio('./assets/sounds/snare.wav'),
-	G: new Audio('./assets/sounds/tom.wav'),
-	H: new Audio('./assets/sounds/tink.wav'),
-}
+const audioElements = {}
+
+// Предзагружаем все звуки
+pads.forEach(pad => {
+	const audio = new Audio(pad.src)
+	audioElements[pad.letter] = audio
+})
 
 // Функция воспроизведения звука
 const playSound = letter => {
@@ -26,9 +33,8 @@ const playSound = letter => {
 
 	// Добавляем визуальный эффект
 	const key = Array.from(keys).find(key => key.querySelector('kbd').textContent === letter)
-	if (!key) return
-	key.classList.add('playing')
-	setTimeout(() => key.classList.remove('playing'), 100)
+	key?.classList.add('playing')
+	setTimeout(() => key?.classList.remove('playing'), 100)
 }
 
 // Обработчик нажатия клавиш
@@ -36,6 +42,8 @@ document.addEventListener('keydown', e => {
 	const letter = e.key.toUpperCase()
 	playSound(letter)
 })
+
+
 
 // Обработчик клика по кнопкам
 keys.forEach(key => {
