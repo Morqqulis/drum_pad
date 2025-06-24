33d3dba922e75d317b6c7af3ba1a62b294dbf6a0
Morqqulis <morqqulis@gmail.com>
2025-06-24 17:42:48 +0400

    123

--- scripts/index.js
+++ scripts/index.js
@@ -2,15 +2,15 @@
 
 const keys = document.querySelectorAll('.key')
 const pads = [
-	{ letter: 'A', src: '/sounds/clap.wav' },
-	{ letter: 'S', src: '/sounds/hihat.wav' },
-	{ letter: 'D', src: '/sounds/kick.wav' },
-	{ letter: 'Z', src: '/sounds/openhat.wav' },
-	{ letter: 'X', src: '/sounds/boom.wav' },
-	{ letter: 'C', src: '/sounds/ride.wav' },
-	{ letter: 'V', src: '/sounds/snare.wav' },
-	{ letter: 'G', src: '/sounds/tom.wav' },
-	{ letter: 'H', src: '/sounds/tink.wav' },
+	{ letter: 'A', src: './sounds/clap.wav' },
+	{ letter: 'S', src: './sounds/hihat.wav' },
+	{ letter: 'D', src: './sounds/kick.wav' },
+	{ letter: 'Z', src: './sounds/openhat.wav' },
+	{ letter: 'X', src: './sounds/boom.wav' },
+	{ letter: 'C', src: './sounds/ride.wav' },
+	{ letter: 'V', src: './sounds/snare.wav' },
+	{ letter: 'G', src: './sounds/tom.wav' },
+	{ letter: 'H', src: './sounds/tink.wav' },
 ]
 
 // Создаем объект для хранения аудио элементов
